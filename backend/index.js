cc4b5e2485d4f433d786d246549540af7bb5f077
parmavictu <victorparma@hotmail.com>
2019-07-15 16:06:25 -0300

    Initial backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const consign = require('consign')
+const mongoose = require('mongoose')
+
+require('./config/mongodb')
+
+app.db = db
+app.mongoose = mongoose
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api/validation.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.listen(3000, () => {
+    console.log('Backend is running on port 3000')
+})
