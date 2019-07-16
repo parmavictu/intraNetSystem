68f04d92663d85666d624f4d4bd8c9852b888567
parmavictu <victorparma@hotmail.com>
2019-07-16 19:42:15 -0300

    auth

--- frontend/src/components/auth/Auth.vue
+++ frontend/src/components/auth/Auth.vue
@@ -1,19 +1,30 @@
 <template>
+    
     <div class="auth-content">
         <div class="auth-modal">
             <div class="auth-title">{{showSignup ? 'Cadastro' : 'Login'}}</div>
-            <input type="text" v-if='showSignup' v-model='user.name' placeholder="Nome">
-            <input type="text"  v-model='user.email' placeholder="E-mail">
-            <input type="text"  v-model='user.password' placeholder="Senha">
-            <input type="text" v-if="showSignup" v-model='user.password' placeholder="Confirme a senha">
+            <hr/>
+            <span class='labels' v-if="showSignup">Nome: </span>
+            <input type="text" v-if='showSignup' v-model='user.name' placeholder="João Gabriel">
+
+        
+            <span class='labels'>E-mail: </span>
+            
+            <input type="text"  class='input-group-text' v-model='user.email' placeholder="joaogabriel@gmail.com">
+            
+            <span class='labels'>Senha: </span>
+            <input type="password"  v-model='user.password' placeholder="********">
+
+            <span class='labels' v-if="showSignup">Confirme a senha: </span>
+            <input type="password" v-if="showSignup" v-model='user.password' placeholder="********">
             
             <button v-if='showSignup'>Registrar</button>
             <button v-else> Fazer Login</button>
 
 
 
-            <span v-if='showSignup'>Já tem cadastro? <a href @click.prevent="showSignup = !showSignup">Acesse o Login!</a></span>
-            <span v-else>Não tem cadastro? <a href @click.prevent="showSignup = !showSignup">Registre-se aqui!</a></span>
+            <span class="button-span" v-if='showSignup'>Já tem cadastro? <a href @click.prevent="showSignup = !showSignup"> Acesse o Login!</a></span>
+            <span v-else class="button-span">Não tem cadastro? <a href @click.prevent="showSignup = !showSignup"> Registre-se aqui!</a></span>
 
 
             <!--<a href @click.prevent='showSignup = !showSignup'>
@@ -37,29 +48,32 @@ export default {
 </script>
 
 <style>
+    
     .auth-content{
         height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
+        
     }
     .auth-modal {
         background-color: #fff;
         width: 350px;
         padding: 35px;
-        box-shadow: 0 1px 5px rgba(0, 0 ,0 , 0.15);
+        box-shadow: 0 2px 6px rgba(0, 0 ,0 , 0.15);
 
         display: flex;
         flex-direction: column;
         align-items: center;
         border-radius: 10px;
+        
     }
 
     .auth-title {
         font-size: 1.2rem;
         font-weight: 100;
         margin-top: 10px;
-        margin-bottom: 15px;
+        margin-bottom: 10px;
     }
     .auth-modal input {
         border: 1px solid #bbb;
@@ -69,7 +83,7 @@ export default {
         outline: none;
         border-radius: 8px;
     }
-    .auth-modal span {
+    .auth-modal .button-span {
         margin-top:35px;
     }
     .auth-modal button{
@@ -83,4 +97,24 @@ export default {
         font-size: 1.2rem;
         background: #ec5252; 
     }
+    .auth-modal .labels {
+        align-self: flex-start;
+        margin-bottom: 8px;
+        font-size: 1.1rem;
+        
+
+    }
+    .auth-modal hr{
+        border: 0;
+        width: 100%;
+        height: 1px;
+        margin-bottom: 15px;
+        background-image: linear-gradient(to right,
+            rgba(120, 120, 120, 0),
+            rgba(120, 120, 120, 0.75),
+            rgba(120, 120, 120, 0));
+    }
+    
+    
+    
 </style>
--- frontend/src/components/templates/Content.vue
+++ frontend/src/components/templates/Content.vue
@@ -13,7 +13,7 @@ export default {
 <style>
     .content{
         grid-area: content;
-        background-color: #efefef;
+        background: #efefef;
         padding: 20px;
     }
 </style>
