a39195c727fa64c8241c3c2f455cf8f92837e54f
parmavictu <victorparma@hotmail.com>
2019-07-19 20:50:49 -0300

    store 1

--- frontend/src/components/templates/UserDropdown.vue
+++ frontend/src/components/templates/UserDropdown.vue
@@ -8,6 +8,7 @@
             <i class="fa fa-angle-down"></i>
         </div>
         <div class="user-dropdown-content">
+            <a href><i class="fa fa-user"></i> Meu Perfil</a>
             <a href><i class="fa fa-cogs"></i> Administração</a>
             <a href ><i class='fa fa-sign-out'></i> Sair </a>
         </div>
@@ -30,54 +31,54 @@ export default {
         position: relative;
         height: 100%;
     }
-
     .user-button {
-        display: flex;
+        display:flex;
         align-items: center;
-        color: #fff;
+        color:#fff;
         font-weight: 100;
         height: 100%;
-        padding: 0 20px;
+        padding: 0px 20px;
+        margin-right: 2px;
     }
-    .user-button:hover {
-        background-color: rgba(0, 0 , 0, 0.2);
+    .user-button:hover{
+        background-color: rgba(0, 0, 0, 0.2);
     }
-    .user-dropdown-img {
-        margin: 0 10px;
+    .user-dropdown-img{
+        margin: 0px 12px;
+        
     }
     .user-dropdown-img > img {
         max-height: 37px;
-        border-radius: 5px;
+        border-radius: 5px; 
     }
     .user-dropdown-content {
         position: absolute;
         right: 0px;
         background-color: #F9F9F9;
-        min-width: 170px;
-        box-shadow: 0px 0px 16px 0px  rgba(0, 0, 0, 0.2);
+        min-width: 170PX;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
         padding: 10px;
-        z-index:1;
+        z-index: 1;
 
         display: flex;
         flex-direction: column;
         flex-wrap: wrap;
 
         visibility: hidden;
-        opacity: 0;
-        transition: visibility 0s, opacity 0.5s linear;
-
+        opacity:0;
+        transition: visibility 0, opacity 0.5s linear;
+        border-radius: 8px;
     }
-    .user-dropdown:hover .user-dropdown-content{
+    .user-dropdown:hover .user-dropdown-content {
         visibility: visible;
         opacity: 1;
     }
     .user-dropdown-content a {
         text-decoration: none;
-        color: #000;
+        color:#000;
         padding: 10px;
-
     }
-    .user-dropdown-content a:hover {
+    .user-dropdown-content a:hover{
         text-decoration: none;
         color: #000;
         background-color: #EDEDED;
---
+++ frontend/src/config/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+        isMenuVisible: false,
+        user:null
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if(!state.user) {
+                state.isMenuVisible = false
+                return
+            }
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            }else {
+                state.isMenuVisible = isVisible
+            }
+        }
+    }
+})
