731340ce7913310ddc30ec57d751a63e94de4c1e
parmavictu <victorparma@hotmail.com>
2019-07-19 12:13:13 -0300

    dropdown

--- frontend/src/components/templates/Header.vue
+++ frontend/src/components/templates/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <header class="header">
-        <a  class="toggle" v-if="!hideToggle"><i class="fa fa-lg" :class='icon'></i></a>
+        <a  class="toggle" ><i class="fa fa-angle-left" ></i></a>
         <h1 class="title"><a href>{{title}}</a></h1>
+        <UserDropdown/>
     </header>
 </template>
 
@@ -22,6 +23,30 @@ export default {
     .header{
         grid-area: header;
         background: linear-gradient(to right, #2c2f45 25%, #161623);
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
-    
+    .title{
+        font-size: 1.2rem;
+        color: #fff;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+    .title a {
+        text-decoration: none;
+        color:#fff
+    }
+    header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: #fff;
+        justify-self: flex-start;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
